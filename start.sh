0f62b03fd8b8a851083f453073e1cd47241a739d
Angelina Pan <angln.pnn@gmail.com>
2025-02-03 18:35:00 +0700

    fixed

--- start.sh
+++ start.sh
@@ -10,6 +10,12 @@ then
     cp "${template_env}"  "${main_env}"
 fi
 
-docker compose -f docker/docker-compose.yml up --build
+#docker compose -f docker/docker-compose.yml up --build
+#docker compose -f docker/docker-compose.yml down
+#exit
+
+docker compose -f docker/docker-compose.yml up -d --build
+poetry run alembic -c src/alembic.ini upgrade head && alembic -c src/alembic.ini stamp head && python src/main.py
+trap SIGINT
 docker compose -f docker/docker-compose.yml down
 exit
